1a5c819e7dc9f0bbd1d6ec10ddf9b9d3c1942c74
sasutthirak <sa.sutthirak@gmail.com>
2026-01-21 12:32:36 +0700

    Update gyax.ino

--- gyax.ino
+++ gyax.ino
@@ -6,11 +6,10 @@ const int sda_pin = 21;
 const int scl_pin = 22;
 
 void setup() {
-  Serial.begin(115200); // ต้องตรงกับ Python
+  Serial.begin(115200); 
   while (!Serial);
   Wire.begin(sda_pin, scl_pin);
   
-  // เริ่มต้น Sensor
   BMI160.begin(BMI160GenClass::I2C_MODE, i2c_addr);
 }
 
@@ -21,7 +20,7 @@ void loop() {
   BMI160.readGyro(gxRaw, gyRaw, gzRaw);
   BMI160.readAccelerometer(axRaw, ayRaw, azRaw);
 
-  // คำนวณ (Manual Calculation)
+  // คำนวณ 
   float ax = axRaw / 16384.0;
   float ay = ayRaw / 16384.0;
   float az = azRaw / 16384.0;
@@ -29,13 +28,13 @@ void loop() {
   float gy = gyRaw / 131.0;
   float gz = gzRaw / 131.0;
 
-  // --- ส่งเป็น JSON (สำคัญมาก!) ---
-  Serial.print("{\"ax\":"); Serial.print(ax, 5);  // <--- เติม , 5
-  Serial.print(", \"ay\":"); Serial.print(ay, 5); // <--- เติม , 5
-  Serial.print(", \"az\":"); Serial.print(az, 5); // <--- เติม , 5
-  Serial.print(", \"gx\":"); Serial.print(gx, 5); // <--- เติม , 5
-  Serial.print(", \"gy\":"); Serial.print(gy, 5); // <--- เติม , 5
-  Serial.print(", \"gz\":"); Serial.print(gz, 5); // <--- เติม , 5
+  // --- ส่งเป็น JSON  ---
+  Serial.print("{\"ax\":"); Serial.print(ax, 5);  
+  Serial.print(", \"ay\":"); Serial.print(ay, 5); 
+  Serial.print(", \"az\":"); Serial.print(az, 5); 
+  Serial.print(", \"gx\":"); Serial.print(gx, 5); 
+  Serial.print(", \"gy\":"); Serial.print(gy, 5); 
+  Serial.print(", \"gz\":"); Serial.print(gz, 5); 
   Serial.println("}");
 
   delay(50); 
